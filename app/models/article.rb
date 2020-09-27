72e39e921ae2591f2c5c32ec51d690d39e6bb6e3
hiro-04 <h.kataoka04@gmai.com>
2020-09-27 23:38:28 +0900

    active_hashの導入（category)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
 class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
   belongs_to :user
   has_many :comments
 
   with_options presence: true do
     validates :title
     validates :content
-    validates :category
+    validates :category_id, numericality: { other_than: 1 }
   end
 end
