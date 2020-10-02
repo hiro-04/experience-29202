496d0f1a37ed8a4a7775de3cd92c6b564ada9dc8
hiro-04 <h.kataoka04@gmai.com>
2020-10-02 15:54:16 +0900

    ルーティング、バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source:followed
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # user validation
   with_options presence: true do
@@ -18,4 +21,25 @@ class User < ApplicationRecord
     validates :email
     validates :birthday
   end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  #現在のユーザーがフォローされていたらtrueを返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   devise_for :users
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :articles do
     resources :comments
   end
