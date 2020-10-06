d506ce399ec289cdd4a055cdd26b8a941519a0fa
hiro-04 <h.kataoka04@gmai.com>
2020-10-06 11:46:36 +0900

    redirect先の変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
 
   def create
     Like.create(user_id: current_user.id, article_id: params[:id])
-    redirect_to root_path
+    redirect_to article_path
   end
 
   def destroy
