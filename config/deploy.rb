cf18677053150d1a472e46ff1bbefc69db1dd5ac
hiro-04 <h.kataoka04@gmai.com>
2020-10-13 19:24:28 +0900

    git修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'experience-29202'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:hiro-04/experience-29202.git'
+set :repo_url,  'https://github.com/hiro-04/experience-29202.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
